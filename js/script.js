2ef195a1c5f9557d526f2721ce1feafc53803f1b
YakirSolomon <yakir@soluto.com>
2018-12-23 18:33:59 +0200

    updating js_conditions

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 
 console.log(“Hello World!”);
+
+var populationArray = []];
+var maxPopulation = 0;
+var isOverPopulated = false;
+
+populationArray = ['50000','800000','700000','1200000','900000' ];
+maxPopulation = 900000;
+
+for(var i=0;i<populationArray.length;i++)
+{
+    if(populationArray[i]>)
+}
