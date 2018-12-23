b5afb46fdd920c95b5a5a6c09d46cd360b2494a4
YakirSolomon <yakir@soluto.com>
2018-12-23 18:44:54 +0200

    update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,17 @@ var isOverPopulated = false;
 populationArray = ['50000','800000','700000','1200000','900000' ];
 maxPopulation = 900000;
 
-for(var i=0;i<populationArray.length;i++)
-{
-    if(populationArray[i]>)
+for(var i=0;i<populationArray.length;i++){
+    if(populationArray[i]>maxPopulation)
+        console.log("larger");
+    else if(populationArray[i]<maxPopulation)
+            console.log("smaller");
+        else    
+            console.log("equals");
 }
+
+if((populationArray[populationArray.length-1]<100000)&&(populationArray[populationArray.length-2]<100000)&&populationArray[populationArray.length-3]<100000)&&(maxPopulation<100000)||(isOverPopulated==false))
+    console.log("Small cities!!!");
+else    
+    console.log("Large cities!!!");
+    
